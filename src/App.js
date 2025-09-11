4e1a6540d0786a1c36bf9e14480f619181c3dbe0
Honeydei Yssabelle Baran Nakagawa <hybnakagawa@addunet.ph>
2025-09-11 19:57:45 +0800

    recreating portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
+import Nav from './nav';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+
+const NavBarLayout = () => (
+  <>
+    <Nav />
+    <Outlet />
+  </>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<NavBarLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,9 @@
+function Portfolio(){
+    return(
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    )
+}
+
+export default Portfolio;
