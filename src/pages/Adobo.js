85793663bfbdaaaa810594843c3d1fb2095923e5
Jethro Carcedo <186544739+TuffestGuy@users.noreply.github.com>
2025-10-02 15:25:42 +0800

    feat: added instructions for adobo recipe

--- src/pages/Adobo.js
+++ src/pages/Adobo.js
@@ -15,23 +15,30 @@ function Adobo(){
         })
     }, []);
 
-     return(
-        <div>
-            <img src={recipe.strMealThumb} alt="Adobo" width="500px" height="500px" className="center-img"/>
-            <div className="recipe-text-container">{recipe.strMeal}</div>
-            <div className="ingredients-container">
-            <h4>These are the ingredients needed:</h4>
-            <ul>
-                {Array.from({length: 20}, (_, i) => {
-                    const ingredient = recipe[`strIngredient${i+1}`];
-                    const measure = recipe[`strMeasure${i+1}`];
-                    return ingredient && ingredient.trim() !== "" ? (
-                        <li key={i}>{measure} {ingredient}</li>
-                    ) : null;
-                })}
-            </ul>
-         </div>
-        </div>
-    )
+   return(
+    <div>
+        <img src={recipe.strMealThumb} alt="Adobo" width="500px" height="500px" className="center-img"/>
+        <div className="recipe-text-container">{recipe.strMeal}</div>
+      <div style={{ display: "flex", gap: "32px", justifyContent: "center", alignItems: "flex-start" }}>
+    <div className="ingredients-container">
+        <h4>These are the ingredients needed:</h4>
+        <ul>
+            {Array.from({length: 20}, (_, i) => {
+                const ingredient = recipe[`strIngredient${i+1}`];
+                const measure = recipe[`strMeasure${i+1}`];
+                return ingredient && ingredient.trim() !== "" ? (
+                    <li key={i}>{measure} {ingredient}</li>
+                ) : null;
+            })}
+        </ul>
+    </div>
+    <div className="instructions-container">
+        <h4>Instructions:</h4>
+        <p>{recipe.strInstructions}</p>
+    </div>
+</div>
+    </div>
+)
 }
+
 export default Adobo
